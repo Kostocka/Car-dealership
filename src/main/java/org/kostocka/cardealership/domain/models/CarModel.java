4cb9a78b0129bebfe7a83ba19d799bd958ec6961
Kostocka <kostapejpo@gmail.com>
2026-03-11 00:48:48 +0300

    add Cars

---
+++ src/main/java/org/kostocka/cardealership/domain/models/CarModel.java
@@ -0,0 +1,44 @@
+package org.kostocka.cardealership.domain.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import org.kostocka.cardealership.domain.models.parts.*;
+import org.kostocka.cardealership.domain.vo.DrivetrainType;
+import org.kostocka.cardealership.domain.vo.Money;
+import org.kostocka.cardealership.domain.vo.id.CarModelId;
+
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class CarModel implements Cloneable
+{
+    private final CarModelId modelId;
+
+    private final String brand;
+    private final String model;
+
+    private final Body body;
+    private final Engine engine;
+    private final GearBox gearBox;
+    private final Interior interior;
+    private final Wheels wheels;
+    private final DrivetrainType drivetrainType;
+    private final String color;
+
+    private final Money basePrice;
+
+    @Override
+    public CarModel clone()
+    {
+        try
+        {
+            return (CarModel) super.clone();
+        }
+        catch (CloneNotSupportedException e)
+        {
+            throw new RuntimeException(e);
+        }
+    }
+}
