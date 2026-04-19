2edb758cd48c0ff42ece8dad7cae37187dedd995
Kostocka <kostapejpo@gmail.com>
2026-04-19 16:27:01 +0300

    add order stock controller

--- src/main/java/peipo/ru/cardealership/domain/repository/StockOrderRepository.java
+++ src/main/java/peipo/ru/cardealership/domain/repository/StockOrderRepository.java
@@ -1,7 +1,11 @@
 package peipo.ru.cardealership.domain.repository;
 
+import java.util.List;
 import peipo.ru.cardealership.domain.models.orders.StockCarOrder;
+import peipo.ru.cardealership.domain.vo.id.ClientId;
 import peipo.ru.cardealership.domain.vo.id.OrderId;
 
 public interface StockOrderRepository extends Repository<StockCarOrder, OrderId>
-{}
+{
+    List<StockCarOrder> findByClientId(ClientId clientId);
+}
