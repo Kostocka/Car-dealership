0b4da1424ef2317151c6e4f3c8f96d966be9cddb
Kostocka <kostapejpo@gmail.com>
2026-05-28 03:06:37 +0300

    add common swagger

---
+++ gateway-service/src/main/java/peipo/ru/gateway/SecurityConfig.java
@@ -0,0 +1,36 @@
+package peipo.ru.gateway;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@Configuration
+@EnableMethodSecurity
+public class SecurityConfig
+{
+    @Bean
+    public SecurityWebFilterChain securityFilterChain(ServerHttpSecurity http)
+    {
+        return http
+                .csrf(ServerHttpSecurity.CsrfSpec::disable)
+                .cors(Customizer.withDefaults())
+
+                .authorizeExchange(exchange -> exchange
+                        .pathMatchers(HttpMethod.OPTIONS).permitAll()
+                        .pathMatchers(
+                                "/swagger/**",
+                                "/v3/api-docs/**",
+                                "/swagger-ui/**"
+                        ).permitAll()
+
+                        .anyExchange().authenticated()
+                )
+                .oauth2ResourceServer(oauth2 ->
+                        oauth2.jwt(Customizer.withDefaults()))
+                .build();
+    }
+}
